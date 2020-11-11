e6bf86517f8e83c3d1862272e7f940c3e6e195d9
Yurii Bykov <jim95022@gmail.com>
2020-11-11 20:54:40 +0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+import os 
 from random import choice
-from cities import cities
 import requests, json
+
+from flask import Flask, render_template
+
+from cities import cities
+
+
+GOOGLE_MAPS_API = os.environ['GOOGLE_MAPS_API']
 
 app = Flask(__name__)
 
@@ -16,12 +22,12 @@ class city():
 @app.route('/')
 def map():
     city.name = choice(cities)[1]
-    url = f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={city.name}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyC9XrXe1rrRiJsBU-ZwalbqCtz2Gy69ZmI'
+    url = f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={city.name}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key={GOOGLE_MAPS_API}'
     city.content = json.loads(requests.get(url).text)
     city.lat = city.content['candidates'][0]['geometry']['location']['lat']
     city.lng = city.content['candidates'][0]['geometry']['location']['lng']
     photo_ref = city.content['candidates'][0]['photos'][0]['photo_reference']
-    city.photo = f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_ref}&key=AIzaSyC9XrXe1rrRiJsBU-ZwalbqCtz2Gy69ZmI'
+    city.photo = f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_ref}&key={GOOGLE_MAPS_API}'
 
     return render_template('map.html', city=city)
 
